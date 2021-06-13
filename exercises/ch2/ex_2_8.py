5d841bf64c809314d2cd63b65a3d7ef0c87daf9c
Eric <franz.eric.schneider@gmail.com>
2021-06-13 14:48:30 -0400

    Really basic detector sort of runs

--- exercises/ch2/ex_2_8.py
+++ exercises/ch2/ex_2_8.py
@@ -1,11 +1,63 @@
 import cv2
 from glob import glob
+from os.path import basename, join
 
+from matplotlib import pyplot
+from matplotlib.patches import Ellipse
 import numpy
 
 
 # Always
 numpy.set_printoptions(suppress=True)
+
+PATH_MAPPING = {
+    "people_1.jpeg": "PROCESSED_people_1.jpeg",
+    "people_2.jpg": "PROCESSED_people_2.jpg",
+    "people_3.jpeg": "PROCESSED_people_3.jpeg",
+    "people_4.jpg": "PROCESSED_people_4.jpg",
+    "people_5.jpg": "PROCESSED_people_5.jpg",
+    "people_6.jpg": "PROCESSED_people_6.jpg",
+    "people_7.jpg": "PROCESSED_people_7.jpg",
+    "people_8.jpeg": "PROCESSED_people_8.jpeg",
+    "people_9.jpg": "PROCESSED_people_9.jpg",
+}
+VISUALIZE_PATH = "ex_2_8_images/DETECTED"
+
+
+def get_samples(search_path):
+    image_paths = glob(search_path)
+    images = [cv2.imread(path) for path in image_paths]
+    xy_images = [pixel_chromaticity(image) for image in images]
+    # TODO: Consider overweighting samples from small images
+    samples, masks = flatten_nonzero(xy_images)
+    return samples, image_paths, masks
+
+
+def from_masks(paths, masks, invert=False):
+    """Returns the raw sample values either in or out of the masks.
+
+    Arguments:
+        paths: list of paths to processed images, each of which should have a
+            unprocessed counterpart
+        masks: list of (n, m) boolean matrices indicating skin locations in the
+            processed image. There should be a 1:1 relationship with paths
+        invert: boolean. When false, return the samples within the masks. When
+            true, return the samples outside the masks
+
+    Returns: (q, 2) array of xy sampled pixel values
+    """
+    samples = None
+    for path, mask in zip(paths, masks):
+        raw_path = get_unprocessed(path)
+        image = cv2.imread(raw_path)
+        xy_image = pixel_chromaticity(image)
+        if invert:
+            mask = numpy.logical_not(mask)
+        if samples is None:
+            samples = xy_image[mask]
+        else:
+            samples = numpy.vstack((samples, xy_image[mask]))
+    return samples
 
 
 # Eq 2.104 on page 88
@@ -14,24 +66,20 @@ RGB_to_XYZ = numpy.array([
     [0.17697, 0.8124, 0.01063],
     [0,       0.01,   0.99   ],
 ])
+XYZ_to_RGB = numpy.linalg.inv(RGB_to_XYZ)
 
-def pixel_chromaticity(image, mask=None):
+def pixel_chromaticity(image):
     """
     Arguments:
         images: (n, m, 3) numpy array, dtype uint8. Color image of the scene.
-        mask: (n, m) numpy array, dtype bool, or None. If bool, indicates the
-            pixels we want to calculate chromaticity for.
 
     Returns: (n, m, 2) numpy array, dtype uint8. (x, y) chromaticity values,
         which were first transferred to (X, Y, Z) space, then normalized to get
         chromaticity coordinates.
     """
-    assert len(image.shape) == 3
-    assert image.shape[-1] == 3
-    assert image.dtype == numpy.uint8
-    if mask is not None:
-        assert image.shape[0:2] == mask.shape
-        assert mask.dtype == bool
+    assert len(image.shape) == 3, "Needs width, height, and color channels"
+    assert image.shape[-1] == 3, "Color channel wasn't 3D"
+    assert image.dtype == numpy.uint8, "Given image wasn't uint8"
 
     # Get XYZ coordinates by applying RGB_to_XYZ matrix to each pixel
     # numpy.einsum is quite complicated and seems quite useful, as a reminder:
@@ -39,13 +87,188 @@ def pixel_chromaticity(image, mask=None):
     # https://stackoverflow.com/questions/25922212/element-wise-matrix-multiplication-in-numpy
     XYZ = numpy.einsum("ij,nmj->nmi", RGB_to_XYZ, image)
     # Normalize into xyz coordinates
+    # TODO: Figure out how to remove the 0/0 warnings
     xyz = numpy.einsum("ijk,ij->ijk", XYZ, 1 / numpy.sum(XYZ, axis=2))
+    # And remove the nans of pure black
+    xyz[numpy.isnan(xyz)] = 0.0
 
     # It's not clear to me why chromaticity is xy instead of xyz, but hey
-    return xyz[:, :, :2].astype(numpy.uint8)
+    return xyz[:, :, :2]
+
+
+def flatten_nonzero(images):
+    """Select the non-zero pixels and flatten it all.
+
+    The pixels that aren't of interest have been painted out using pure block,
+    which is why this is done.
+
+    Arguments:
+        images: list of (n, m, 2) images in xy coordinates.
+
+    Returns: two-element tuple of
+        [0] (p, 2) vector flattened down from all of the images.
+        [1] Lists of the masks the the samples were extracted with,
+            corresponding to the incoming images
+    """
+    flattened = None
+    masks = []
+    for image in images:
+        # Get the locations where either x or y pixels have any color value
+        x0 = image[:, :, 0] != 0.0
+        y0 = image[:, :, 1] != 0.0
+        nonzeros = x0 | y0
+        # Then add the latest nonzero pixels to the growing list
+        if flattened is None:
+            flattened = image[nonzeros]
+        else:
+            flattened = numpy.vstack((flattened, image[nonzeros]))
+        # Capture the masks
+        masks.append(nonzeros.copy())
+    return flattened, masks
+
+
+def plot(samples, covariance=None, background=None, title=""):
+
+    # Set up the shared axes
+    left = bottom = 0.1
+    width = height = 0.65
+    spacing = 0.005
+    figure = pyplot.figure(figsize=(8, 8))
+    axis = figure.add_axes([left, bottom, width, height])
+    axis_histx = figure.add_axes([left, bottom + height + spacing, width, 0.2],
+                                 sharex=axis)
+    axis_histy = figure.add_axes([left + width + spacing, bottom, 0.2, height],
+                                 sharey=axis)
+    axis_histx.tick_params(axis="x", labelbottom=False)
+    axis_histy.tick_params(axis="y", labelleft=False)
+
+    # Plot the distribution
+    if background is not None:
+        axis.scatter(background[:, 0], background[:, 1], s=0.2, color="k",
+                     label="background")
+    axis.scatter(samples[:, 0], samples[:, 1], s=0.3, label="samples")
+    axis_histx.hist(samples[:, 0], bins=100)
+    axis_histy.hist(samples[:, 1], bins=100, orientation="horizontal")
+
+    # And the covariance on top of it
+    if covariance is not None:
+        means = numpy.mean(samples, axis=0)
+        axis.scatter(means[0], means[1], s=10, color="r", label="mean")
+        # Get the main axes
+        values, vectors = numpy.linalg.eig(covariance)
+        std_x, std_y = numpy.sqrt(values)
+        angle = numpy.arctan2(vectors[1, 0], vectors[0, 0])
+        axis.add_artist(Ellipse(means, values[0], values[1], angle, color="c",
+                                fill=False, label="cov"))
+        axis.add_artist(Ellipse(means, std_x, std_y, angle, color="r",
+                                fill=False, label="sqrt(cov)"))
+
+    # Add labels
+    axis.legend()
+    axis.set_xlabel("x value (normalized X)")
+    axis.set_ylabel("y value (normalized Y)")
+    axis_histx.set_title(title)
+    pyplot.show()
+
+
+def get_unprocessed(path):
+    """Get the unprocessed version of the given processed path."""
+    for raw_path in glob("ex_2_8_images/*jp*g"):
+        if PATH_MAPPING.get(basename(raw_path), None) == basename(path):
+            break
+    else:
+        raise RuntimeError(f"Couldn't find raw image for {path}")
+    return raw_path
+
+
+def detect_skin(image, mean, axes, stddev_values, scalar=1.0):
+    """Checks whether each xy pixel is within the detected range for "skin".
+
+    In practice this makes a rectangle around the mean, along the axes. Maybe
+    it should be changed to an ellipse?
+
+    Arguments:
+        image: (n, m, 2) numpy array, dtype float. This should contain the xy
+            units for the image
+        mean: length-2 vector in the xy units, indicating the center of the
+            rectangle
+        axes: (2, 2) matrix, where each column is an eigenvector indicating a
+            major axis of the detected covariance
+        stddev_values: length of the rectangle sides, corresponding in order to
+            the axes.
+        scalar: float, default 1. This is multiplied by the stddev to either
+            grow or shrink the rectangle from being one standard deviation.
+
+    Returns: (n, m) boolean array, indicating whether that pixel is in the
+        indicated rectangle.
+    """
+
+    # Get the full image in xy coordinates
+    xy_image = pixel_chromaticity(image)
+    # Center the values around the mean
+    xy_image -= mean
+    # Check the distance along the major and minor axes
+    within_axes0 = numpy.abs(
+        numpy.einsum("ijk,k->ij", xy_image, axes[:, 0])
+    ) < scalar * stddev_values[0]
+    within_axes1 = numpy.abs(
+        numpy.einsum("ijk,k->ij", xy_image, axes[:, 1])
+    ) < scalar * stddev_values[1]
+    # "Skin" are those values within both axes directions
+    return within_axes0 & within_axes1
+
+
+def visualize_mask(path, mask, suffix):
+    # To visualize the mask just write the True values to red
+    image = cv2.imread(path)
+    if mask is not None:
+        image[mask] = [0, 0, 255]
+    # Then save the file with an altered name
+    filename = basename(path)
+    filename = filename.split(".")
+    filename = f"{filename[0]}_{suffix}.{filename[-1]}"
+    cv2.imwrite(join(VISUALIZE_PATH, filename), image)
+
+
+def main():
+    samples, paths, masks = get_samples("ex_2_8_images/PROCESSED/*jp*g")
+    mean = numpy.mean(samples, axis=0)
+    # Get covariance and the main axes
+    covariance = numpy.cov(samples.T)
+    cov_values, vectors = numpy.linalg.eig(covariance)
+    # Hand-tuned scalar around the covariance (very unscientific)
+    scalar = 3
+    # THIS ENDED UP BEING TOO BROAD
+    # Sqrt of variance is std deviation, so we can express ourselves in
+    # multiples of stddev
+    # stddev_values = numpy.sqrt(cov_values)
+
+    # # Optional visualization
+    # all_pixels, _, _ = get_samples("ex_2_8_images/*jp*g")
+    # plot(samples, covariance, all_pixels,
+    #      title="x,y values for skin pixel samples on background")
+
+    # # Alternate visualization
+    # plot(from_masks(paths, masks, invert=False),
+    #      title="x,y values for skin pixel samples")
+    # plot(from_masks(paths, masks, invert=True),
+    #      title="x,y values for BACKGROUND samples")
+
+    # Visualize images for which there is ground truth
+    for path, mask in zip(paths, masks):
+        raw_path = get_unprocessed(path)
+        detected = detect_skin(cv2.imread(raw_path), mean, vectors, cov_values, scalar)
+        visualize_mask(raw_path, None, "ORIGINAL")
+        visualize_mask(raw_path, detected, "DETECTED")
+        visualize_mask(raw_path, mask, "GROUND_TRUTH")
+
+    # And those without ground truth
+    for raw_path in glob("ex_2_8_images/raw_only*jp*g"):
+        detected = detect_skin(cv2.imread(raw_path), mean, vectors, cov_values, scalar)
+        visualize_mask(raw_path, None, "ORIGINAL")
+        visualize_mask(raw_path, detected, "DETECTED")
+
 
 
 if __name__ == "__main__":
-
-    image_paths = glob("ex_2_8_images/PROCESSED/*")
-    images = [cv2.imread(path) for path in image_paths]
+    main()
--- exercises/ch2/ex_2_8_test.py
+++ exercises/ch2/ex_2_8_test.py
@@ -1,7 +1,70 @@
+from unittest import mock
 import numpy
 import pytest
 
-from ex_2_8 import pixel_chromaticity
+from ex_2_8 import (
+    detect_skin,
+    flatten_nonzero,
+    from_masks,
+    pixel_chromaticity,
+)
+
+
+@mock.patch("ex_2_8.pixel_chromaticity")
+@mock.patch("cv2.imread")
+@mock.patch("ex_2_8.get_unprocessed")
+def test_from_masks(get_mock, imread_mock, chromaticity_mock):
+
+    # These are the images we want to operate on (2, 2, 2)
+    def images():
+        return [
+            numpy.array([
+                [[0.5, 0.5], [0.25, 0.7]],
+                [[0.1, 0.9], [0.9, 0.9]],
+            ]),
+            numpy.array([
+                [[1.5, 1.5], [1.25, 1.7]],
+                [[1.1, 1.9], [1.9, 1.9]],
+            ]),
+        ]
+    chromaticity_mock.side_effect = images()
+
+    # These paths will do nothing, just check they get used
+    paths = ["A", "B"]
+    # Build the masks to return certain image pixels
+    masks = [
+        numpy.array([
+            [True, False],
+            [False, True],
+        ]),
+        numpy.array([
+            [True, False],
+            [True, False],
+        ]),
+    ]
+
+    # First check the invert=False case
+    samples = from_masks(paths=paths, masks=masks, invert=False)
+
+    # Check that the paths are used
+    assert get_mock.call_args_list == [mock.call("A"), mock.call("B")]
+    # And that the samples are right
+    assert numpy.all(samples == numpy.array([
+        [0.5, 0.5],
+        [0.9, 0.9],
+        [1.5, 1.5],
+        [1.1, 1.9],
+    ]))
+
+    # Check again for invert=True
+    chromaticity_mock.side_effect = images()
+    samples = from_masks(paths=paths, masks=masks, invert=True)
+    assert numpy.all(samples == numpy.array([
+        [0.25, 0.7],
+        [0.1, 0.9],
+        [1.25, 1.7],
+        [1.9, 1.9],
+    ]))
 
 
 @pytest.fixture
@@ -9,7 +72,7 @@ def image():
     """Makes a (2, 2, 3) color image, dtype uint8."""
     return numpy.array([
         [
-            [1, 2, 3],
+            [0, 0, 0],
             [10, 11, 12]
         ],
         [
@@ -21,30 +84,16 @@ def image():
 
 class TestPixelChromaticity:
 
-    @pytest.mark.parametrize("image,mask", (
+    @pytest.mark.parametrize("image", (
         # Not color images
-        (numpy.ones((5, 5)).astype(numpy.uint8), None),
-        (numpy.ones((5, 5, 1)).astype(numpy.uint8), None),
+        numpy.ones((5, 5)).astype(numpy.uint8),
+        numpy.ones((5, 5, 1)).astype(numpy.uint8),
         # Wrong dtype (float, not uint8)
-        (numpy.ones((5, 5, 3)), None),
-        # Mask size mismatches
-        (
-            numpy.ones((5, 5, 3)).astype(numpy.uint8),
-            numpy.ones((6, 6)).astype(bool),
-        ),
-        (
-            numpy.ones((5, 5, 3)).astype(numpy.uint8),
-            numpy.ones((5, 5, 1)).astype(bool),
-        ),
-        # Wrong mask dtype (float, not bool)
-        (
-            numpy.ones((5, 5, 3)).astype(numpy.uint8),
-            numpy.ones((5, 5)),
-        ),
+        numpy.ones((5, 5, 3)),
     ))
-    def test_failures(self, image, mask):
+    def test_failures(self, image):
         with pytest.raises(AssertionError):
-            pixel_chromaticity(image, mask)
+            pixel_chromaticity(image)
 
     def test_values(self, image):
         """Check that the value mapping operates as expected."""
@@ -52,7 +101,7 @@ class TestPixelChromaticity:
         # XYZ values corresponding to the image fixture
         XYZ = numpy.array([
             [
-                [1.72, 1.83366, 2.99],
+                [0, 0, 0],
                 [10.72, 10.83366, 11.99],
             ],
             [
@@ -65,12 +114,106 @@ class TestPixelChromaticity:
 
         # Check basic type assertions
         assert xy.shape == (2, 2, 2)
-        assert xy.dtype == numpy.uint8
+        assert xy.dtype == float
 
         # Check the chromaticity calculation
         for i in range(2):
             for j in range(2):
-                assert xy[i, j, 0] == int(XYZ[i, j, 0] / numpy.sum(XYZ[i, j]))
-                assert xy[i, j, 1] == int(XYZ[i, j, 1] / numpy.sum(XYZ[i, j]))
+                if numpy.sum(XYZ[i, j]) == 0.0:
+                    # Check that the chromaticity of full black pixels is
+                    # marked as 0 instead of nan
+                    assert xy[i, j, 0] == 0.0
+                    assert xy[i, j, 1] == 0.0
+                else:
+                    assert numpy.isclose(xy[i, j, 0],
+                                         XYZ[i, j, 0] / numpy.sum(XYZ[i, j]))
+                    assert numpy.isclose(xy[i, j, 1],
+                                         XYZ[i, j, 1] / numpy.sum(XYZ[i, j]))
 
-    # TODO: Test masks
+
+def test_flatten_nonzero():
+    # Make some appropriate fake images. Full-zero pixels should be left out
+    # in the end, pixels where one or more element is non-zero should be
+    # included
+    images = [
+        numpy.array([
+            [[0.0, 0.0], [0.0, 1.0], [0.5, 1.0]],
+            [[0.2, 0.8], [0.9, 0.0], [0.0, 0.0]],
+        ]),
+        numpy.array([
+            [[2.0, 0.0], [0.0, 0.0], [0.5, 0.0], [1.5, 0.4]],
+            [[0.1, 0.0], [0.0, 0.0], [0.0, 0.7], [0.0, 0.0]],
+            [[0.0, 0.0], [1.1, 0.3], [0.0, 0.0], [0.0, 0.2]],
+        ]),
+    ]
+    samples, masks = flatten_nonzero(images)
+    assert numpy.all(samples == numpy.array([
+        [0.0, 1.0],
+        [0.5, 1.0],
+        [0.2, 0.8],
+        [0.9, 0.0],
+        [2.0, 0.0],
+        [0.5, 0.0],
+        [1.5, 0.4],
+        [0.1, 0.0],
+        [0.0, 0.7],
+        [1.1, 0.3],
+        [0.0, 0.2],
+    ]))
+    assert len(masks) == 2
+    assert numpy.all(masks[0] == numpy.array([
+            [0, 1, 1],
+            [1, 1, 0],
+        ],
+        dtype=bool,
+    ))
+    assert numpy.all(masks[1] == numpy.array([
+            [1, 0, 1, 1],
+            [1, 0, 1, 0],
+            [0, 1, 0, 1],
+        ],
+        dtype=bool,
+    ))
+
+
+@mock.patch("ex_2_8.pixel_chromaticity", lambda x: x)
+def test_detect_skin():
+    image = numpy.array([
+        [  # clearly in    clearly out
+            [0.75, 0.75], [0.50, 0.75], [0.75, 0.50],
+        ],
+        [  # in on one axis, out on the other
+            [0.90, 0.90], [0.90, 0.60], [0.60, 0.90],
+        ],
+        [  # in on the main axes, then out
+            [0.95, 0.75], [0.75, 0.55], [0.75, 0.52],
+        ],
+    ])
+
+    mean = numpy.array([0.75, 0.75])
+
+    # Make up some eigenvector axes that are simple to reason about but off
+    # axis. Remember, vectors[:, i] should be an eigenvector. Eigenvectors are
+    # normalized to length 1
+    axes = numpy.array([
+        [1,  1],
+        [1, -1],
+    ], dtype=float)
+    axes[:, 0] /= numpy.linalg.norm(axes[:, 0])
+    axes[:, 1] /= numpy.linalg.norm(axes[:, 1])
+    # These values are the "bounds" within which points should fall. The sqrt(2)
+    # is so we can reason about diagonals, and the 1e-6 is to get edge points in
+    values = numpy.array([0.1, 0.2]) * numpy.sqrt(2) + 1e-6
+
+    # Detect and check
+    skin_mask = detect_skin(image=image, mean=mean, axes=axes, stddev_values=values)
+
+    # Type checks
+    assert skin_mask.shape == (3, 3)
+    assert skin_mask.dtype == bool
+
+    assert numpy.all(skin_mask == numpy.array([
+        [True, False, False],
+        [False, True, True],
+        [True, True, False],
+    ]))
