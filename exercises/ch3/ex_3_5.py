376b944ddd47e1eb74b44607fd4f86d5198d3523
Eric <franz.eric.schneider@gmail.com>
2021-08-12 15:13:26 -0400

    Construct the equation matrices

--- exercises/ch3/ex_3_5.py
+++ exercises/ch3/ex_3_5.py
@@ -15,7 +15,7 @@ def phi(s, p, N, i, k):
     """Calculates phi_{i,k} as from eq. (20) in the linear prediction primer.
 
     From eq. (20) we know that
-    phi_{i,k} = sum {n from 0 to N-1} ( s[n-i] - s[n-k] )
+        phi_{i,k} = sum {n from 0 to N-1} ( s[n-i] - s[n-k] )
 
     It's important to note that i and k can go from 0 to a value called p. So
     in the case where (n=0, i=p), then we have the case s[-p]. This does NOT
@@ -27,6 +27,10 @@ def phi(s, p, N, i, k):
         -5, -4, -3, -2, -1, 0  # We know s must be defined from -p to N-1
          0,  1,  2,  3,  4, 5  # So when we zero-index the array [p] hits our
                                # desired 0 point
+
+    It's important to note that the choices here have been made such that
+    new values should show up in the FRONT of the array, which is a little
+    unusual but I think matches well with the paper's layout.
 
     Arguments:
         s: A list of historical "output signal" values, which I am interpreting
@@ -58,5 +62,43 @@ def phi(s, p, N, i, k):
     for n in range(p, N + p):
         summed += s[n-i] * s[n-k]
 
-    # Standardize the return type
-    return int(summed)
+    return summed
+
+
+def construct_equations(s, p, N):
+    """As shown in eq. (18) we need to make an aX=b matrix and results vector.
+
+    This is the prelude to computing the p different coefficients by solving
+    for p separate linear equations. As eq. (18) states, we want to calculate
+    each row like so:
+        sum {k from 1 to p} a_k * phi(k, i) = -phi(0, i)
+    And then i is defined from 1 to p as well, each i forming a new row. It's
+    pretty clear that we can separate this out into a matrix X of all the LHS
+    phi() components, an "a" vector of a_1 to a_p, then a "b" vector on the RHS
+    that represents the result in this solution.
+
+    Arguments:
+        s: A list of historical "output signal" values, which I am interpreting
+            as the actually-seen pixel values. Which makes this a time series
+            of uint8.
+        p: (int) Number of steps that we need to know "in the future", see
+            docstring equation discussion of phi() function
+        N: (int) Number of steps to look back in the past, needed by phi()
+    """
+
+    matrix = []
+    vector = []
+
+    # Form the rows
+    for i in range(1, p + 1):
+        row = []
+        # And the columns within it. Note the switching of i and k, that was
+        # on purpose and matches the notation in the paper. Not strictly
+        # necessary but it's nice to keep everything consistent.
+        for k in range(1, p + 1):
+            row.append(phi(s=s, p=p, N=N, i=k, k=i))
+        matrix.append(row)
+        vector.append(-1 * phi(s=s, p=p, N=N, i=0, k=i))
+
+    # And put these in numpy
+    return (numpy.array(matrix), numpy.array(vector))
--- exercises/ch3/ex_3_5_test.py
+++ exercises/ch3/ex_3_5_test.py
@@ -1,7 +1,7 @@
 import numpy
 import pytest
 
-from ex_3_5 import phi
+from ex_3_5 import construct_equations, phi
 
 
 class TestPhi:
@@ -45,7 +45,7 @@ class TestPhi:
         def check(s):
             output = phi(s=s, p=3, N=5, i=0, k=0)
             assert output == 54
-            assert isinstance(output, int)
+            assert isinstance(output, int) or isinstance(output, numpy.int64)
 
         # Known working set
         s = [1, 9, 5, 3, 2, 6, 1, 2]
@@ -53,3 +53,42 @@ class TestPhi:
 
         # Try it again with s as an array of uint8
         check(numpy.array(s, dtype=numpy.uint8))
+
+    def test_overlong_s(self):
+        """
+        When s is longer than necessary we want to use the FIRST p+N values,
+        because we have constructed phi such that new values should appear at
+        the beginning of the array.
+        """
+
+        # Expected = 1*3 + 2*4 + 3*5 + 4*6 = 50, which doesn't use the full s
+        assert phi(s=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], p=2, N=4, i=2, k=0) == 50
+
+
+class TestConstructEquations:
+    def test_simple_scenario(self):
+        """Run a simple version of eq. (18) by hand and check it."""
+        matrix, vector = construct_equations(
+            s=numpy.array([1, 7, 4, 2, 6, 3, 1, 7, 4, 3], dtype=numpy.uint8),
+            p=3,
+            N=4,
+        )
+        assert isinstance(matrix, numpy.ndarray)
+        assert isinstance(vector, numpy.ndarray)
+        assert matrix.dtype == numpy.int64
+        assert vector.dtype == numpy.int64
+
+        assert numpy.allclose(
+            matrix,
+            numpy.array([
+                [16 + 4 + 36 + 9 , 28 + 8 + 12 + 18, 4 + 14 + 24 + 6],
+                [28 + 8 + 12 + 18, 49 + 16 + 4 + 36, 7 + 28 + 8 + 12],
+                [4 + 14 + 24 + 6 ,  7 + 28 + 8 + 12, 1 + 49 + 16 + 4]
+            ])
+        )
+        assert numpy.allclose(
+            vector,
+            numpy.array([-(4*2 + 2*6 + 6*3 + 3*1),
+                         -(7*2 + 4*6 + 2*3 + 6*1),
+                         -(1*2 + 7*6 + 4*3 + 2*1)])
+        )
